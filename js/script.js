452f585b6f7027aec64ac166d408927e9b9e407a
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-11-28 19:28:45 -0500

    Finished JS YAY

--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
 var eventType = prompt("Enter the type of event you are attending:");
 var tempFahr = prompt("Enter the current temperature:");
-var result = ("Since it is") + (" ") + tempFahr + (" ") + ("degrees and you are going to something") + (" ") + eventType + (" ") + ("you should wear");
+var clothingSuggestion = " ";
+var becauseTemperature = " ";
+var beforeResult = "Since it is " + tempFahr + " degrees and you are going to a " + eventType + " event, you should wear";
 
-if(eventType === "casual" && tempFahr < 54)
-{
-  console.log(result + (" ") + ("something comfy. Like a coat!")) 
-}else if( eventType === "casual" && tempFahr >= 54 && tempFahr <= 70)
-{
-    console.log(result + (" ") + ("something comfy. Like a jacket!")) 
-}else if( eventType === "casual" && tempFahr > 70)
-{
-    console.log(result + (" ") + ("something comfy. No jacket!"))
-}else if(eventType === "semi-formal" && tempFahr < 54) 
-{
-  console.log(result + (" ") + ("a polo and a coat."))
-}else if( eventType === "semi-formal" && tempFahr >= 54 && tempFahr <= 70)
-{
-  console.log (result + (" ") + ("a polo and a jacket."))
-}else if( eventType === "semi-formal" && tempFahr > 70)
-{
-    console.log(result + (" ") + ("a polo, no jacket."))
-}else if(eventType === "formal" && tempFahr < 54) 
-{
-    console.log(result + (" ") + ("a suit and a coat."))
-}else if(eventType === "formal" && tempFahr >= 54 && tempFahr <= 70)
-{
-    console.log(result + (" ") + ("a suit and a jacket."))
-}else if(eventType === "formal" && tempFahr > 70)
-{
-    console.log(result + (" ") + ("a suit, no jacket."))
+if(eventType === "casual" && tempFahr < 54) {
+  clothingSuggestion = " something comfy.";
+  becauseTemperature = " Like a coat!";
+}else if( eventType === "casual" && tempFahr >= 54 && tempFahr <= 70) {
+    clothingSuggestion = " something comfy.";
+    becauseTemperature = " A jacket perhaps!";
+}else if( eventType === "casual" && tempFahr > 70) {
+    clothingSuggestion = " something comfy.";
+    becauseTemperature = " No jacket needed!";
+}else if(eventType === "semi-formal" && tempFahr < 54) {
+  clothingSuggestion = " a polo ";
+  becauseTemperature = "and a coat.";
+}else if( eventType === "semi-formal" && tempFahr >= 54 && tempFahr <= 70) {
+  clothingSuggestion = " a polo ";
+  becauseTemperature = "and a jacket.";
+}else if( eventType === "semi-formal" && tempFahr > 70) {
+    clothingSuggestion =  " a polo.";
+    becauseTemperature = " No jacket needed!";
+}else if(eventType === "formal" && tempFahr < 54) {
+    clothingSuggestion = " a suit";
+    becauseTemperature = " and a coat.";
+}else if(eventType === "formal" && tempFahr >= 54 && tempFahr <= 70) {
+    clothingSuggestion = " a suit";
+    becauseTemperature = " and a jacket.";
+}else if(eventType === "formal" && tempFahr > 70) {
+    clothingSuggestion = " a suit.";
+    becauseTemperature = " No jacket needed!";
+}else{
+    clothingSuggestion = " SOME CLOTHES PLEASE,";
+    becauseTemperature = " BECAUSE IT'S THE RIGHT THING TO DO!"
 }
+
+var result = beforeResult + clothingSuggestion + becauseTemperature;
+console.log(result);
